b39fcf2b27e42c12303fd59659ee5adc05314eb9
Truong Quang <quangtruong2717@gmail.com>
2026-06-06 22:15:30 +0700

    feat(common): add api response wrappers

---
+++ src/main/java/com/quangtruong/library_management/dto/ApiResponse.java
@@ -0,0 +1,19 @@
+package com.quangtruong.library_management.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+
+public class ApiResponse<T> {
+	Integer code;
+	String message;
+	T data;
+}
